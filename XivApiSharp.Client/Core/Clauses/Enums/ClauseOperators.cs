212f4e51952afd436b9dafe74ed1ae568fbbdffc
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-04-14 19:30:38 +0000

    docs: add documentation for ClauseOperators.cs

--- XivApiSharp.Client/Core/Clauses/Enums/ClauseOperators.cs
+++ XivApiSharp.Client/Core/Clauses/Enums/ClauseOperators.cs
@@ -1,5 +1,9 @@
 namespace XivApiSharp.Client.Core.Clauses.Enums;
 
+/// <summary>
+/// Represents the operation that the clause will use to compare its value to.
+/// </summary>
+/// <seealso cref="Clause{T}"/>
 public enum ClauseOperators
 {
     PartiallyEqualTo,
