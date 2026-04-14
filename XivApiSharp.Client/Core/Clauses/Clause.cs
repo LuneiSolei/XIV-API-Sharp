22cdcbd4432fcca8b4ed495e5f83f413ee42973d
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-04-14 20:35:40 +0000

    docs: add docstrings for Clause.cs and IClause.cs
    - change members to be nullable

--- XivApiSharp.Client/Core/Clauses/Clause.cs
+++ XivApiSharp.Client/Core/Clauses/Clause.cs
@@ -2,18 +2,28 @@ using XivApiSharp.Client.Core.Clauses.Interfaces;
 
 namespace XivApiSharp.Client.Core.Clauses;
 
+/// <inheritdoc />
 public class Clause<T> : IClause where T : notnull
 {
-    public string Specifier { get; set; }
-    public string Operator { get; set; }
-    public virtual T Value { get; set; }
+    /// <inheritdoc />
+    public string? Specifier { get; set; }
 
-    object IClause.Value
+    /// <inheritdoc />
+    public string? Operator { get; set; }
+    
+    /// <summary>
+    /// See: <see cref="IClause.Value"/>
+    /// </summary>
+    public virtual T? Value { get; set; }
+
+    /// <inheritdoc />
+    object? IClause.Value
     {
         get => Value;
-        set => Value = (T)value;
+        set => Value = (T?)value;
     }
 
+    /// <inheritdoc cref="IClause.ToString" />
     public override string ToString()
     {
         return $"{Specifier}{Operator}{Value}";
--- XivApiSharp.Client/Core/Clauses/Interfaces/IClause.cs
+++ XivApiSharp.Client/Core/Clauses/Interfaces/IClause.cs
@@ -1,11 +1,34 @@
 namespace XivApiSharp.Client.Core.Clauses.Interfaces;
 
-public interface IClause
+/// <summary>
+/// Defines a clause (filter/condition) as defined by the XIV API QueryString,
+/// combining a field specifier, comparison operator, and value.
+/// </summary>
+/// <remarks>
+/// Implementations of <see cref="IClause"/> are expected to provide a strongly-typed <c>Value</c>
+/// property, exposing it as <see cref="object"/> for use in generic contexts.
+/// </remarks>
 internal interface IClause
 { 
-    string Specifier { get; set; }
-    string Operator { get; set; }
+    /// <summary>
+    /// Name of the field to compare the value against.
+    /// </summary>
+    string? Specifier { get; set; }
     
-    object Value { get; set; }
+    /// <summary>
+    /// The comparison operator to use.
+    /// </summary>
+    string? Operator { get; set; }
+    
+    /// <summary>
+    /// The boxed value to compare.
+    /// </summary>
+    object? Value { get; set; }
+    
+    /// <summary>
+    /// Converts the specifier, operator, and value of this instance into its
+    /// string representation.
+    /// </summary>
+    /// <returns>The string representation of this instance.</returns>
     string ToString();
 }
