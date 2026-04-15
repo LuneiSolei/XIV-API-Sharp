ff8cf327922c4048bfd77f1690553936ae17cc0f
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-04-15 21:19:56 +0000

    fix: convert boolean clause values to all lowercase when stringifying Clause<T>

--- XivApiSharp.Client/Core/Clauses/Clause.cs
+++ XivApiSharp.Client/Core/Clauses/Clause.cs
@@ -24,6 +24,11 @@ public class Clause<T> : IClause where T : notnull
     /// <inheritdoc cref="IClause.ToString" />
     public override string ToString()
     {
-        return $"{Specifier}{Operator}{Value}";
+        // Convert boolean values to lowercase because .NET capitalizes them by default.
+        string newValue = Value is bool b
+            ? b.ToString().ToLowerInvariant()
+            : Value?.ToString() ?? string.Empty;
+            
+        return $"{Specifier}{Operator}{newValue}";
     }
 }
