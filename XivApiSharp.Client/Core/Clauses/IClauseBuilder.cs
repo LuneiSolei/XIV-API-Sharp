8b14098bc20412a02de1a7fe389380ca80855d9b
lunei-solei <68037318+LuneiSolei@users.noreply.github.com>
2026-04-17 01:49:39 +0000

    docs: add docstring to IClauseBuilder

--- XivApiSharp.Client/Core/Clauses/IClauseBuilder.cs
+++ XivApiSharp.Client/Core/Clauses/IClauseBuilder.cs
@@ -1,7 +1,9 @@
 namespace XivApiSharp.Client.Core.Clauses;
 
+/// <summary>
+/// Represents a type used to build clauses in a <see cref="QueryString"/>.
+/// </summary>
 public interface IClauseBuilder : IInitialClauseBuilderStep, IConditionalStep,
     IOperatorStep
 {
-    
 }
